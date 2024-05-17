363eb8d784e958132ba3cbc01e01388243edae2c
a1120960 <77258073+a1120960@users.noreply.github.com>
2024-05-17 15:28:21 +0930

    updated the index.html file

---
+++ SWAP-PLOT-MAIN.py
@@ -0,0 +1,272 @@
+"""
+Title: SWAP Data Plotting Script
+Author: Ben White
+Date: 2024
+Description: 
+This script reads SWAP data from a CSV file, filters the data, and generates an interactive 
+plot using Plotly. The plot includes markers for different TAU values, a shaded region for 
+lab clocks, and a QR code for more information.
+
+Usage:
+- Ensure the SWAP_DATA.csv file is in the same directory as this script.
+- The QRlink.png image file should also be in the same directory.
+- Run the script to generate an HTML file with the plot. (can be changed to other formats)
+
+Dependencies:
+- os
+- plotly
+- numpy
+- pandas
+- base64
+"""
+
+
+# Import necessary libraries
+import os
+import plotly.graph_objs as go
+import plotly.io as pio
+import plotly.express as px
+import numpy as np
+import pandas as pd
+import base64
+
+# Import SWAP data into a pandas DataFrame
+csv_file = 'SWAP_DATA.csv'
+df = pd.read_csv(csv_file)
+
+# Filter the DataFrame to include only rows where INCLUDE == 1
+df = df[df['INCLUDE'] == 1]
+
+# Filter data for rows that have valid TAU_1e3s and TAU_1e4s values
+df_tau_1e3 = df[df['TAU_1e3s'] > 0]
+df_tau_1e4 = df[df['TAU_1e4s'] > 0]
+
+# Set the default plot template
+pio.templates.default = "seaborn"
+
+# Create a new Plotly figure
+fig = go.Figure()
+
+# Add a horizontal line representing GPS
+horizontal_line = go.Scatter(
+    x=[1e-3, 5e16],
+    y=[4e-12, 4e-12],
+    mode='lines',
+    line=dict(color='black', width=0.5, dash='dot'), 
+    name='GPS',
+    showlegend=False,
+    hoverinfo='skip'
+)
+fig.add_trace(horizontal_line)
+
+# Add scatter plots for TAU_1e4s and TAU_1e3s with vertical lines
+for df_temp, tau_column, symbol_shape in zip([df_tau_1e4, df_tau_1e3], ['TAU_1e4s', 'TAU_1e3s'], ['circle', 'square']):
+    legend_text = 'TAU = 10,000s' if tau_column == "TAU_1e4s" else 'TAU = 1000s'
+    scatter = go.Scatter(
+        x=df_temp['SWAP'],
+        y=df_temp[tau_column],
+        mode='markers',
+        name=legend_text,
+        opacity=0.75,
+        marker=dict(size=8, color='DarkSlateGrey', symbol=symbol_shape),
+        hovertemplate='<b>CLOCK</b>: %{text}<br><b>SWaP</b> = %{x}<br><b>Tau</b> = %{y}<br>',
+        text=df_temp['CLOCK']
+    )
+    fig.add_trace(scatter)
+
+    # Add vertical lines to join the ADEVs for each clock
+    unique_x_values = df_temp['SWAP'].unique()
+    for x_value in unique_x_values:
+        y1 = df[df['SWAP'] == x_value]['TAU_1s'].iloc[0]
+        y2 = df_temp[df_temp['SWAP'] == x_value][tau_column].iloc[0]
+        vertical_line = go.Scatter(
+            x=[x_value, x_value],
+            y=[y1, y2],
+            mode='lines',
+            line=dict(color='black', width=2),
+            showlegend=False,
+            opacity=0.75
+        )
+        fig.add_trace(vertical_line)
+
+# Add the scatter plot for TAU_1s
+scatter_tau1s = px.scatter(
+    df, 
+    x="SWAP", 
+    y="TAU_1s", 
+    color="CATEGORY",
+    symbol='TYPE',
+    hover_name='CLOCK', 
+    hover_data={
+        'ATOM': True,
+        'GROUP': True,
+        'REF_NUM': True,
+        'SWAP': True,
+        'TAU_1s': True,
+        'CATEGORY': True,
+        'TYPE': True,
+        'CLOCK': False
+    },
+    labels={
+        'CLOCK': '<b>Clock</b>',
+        'CATEGORY': '<b>Category</b>',
+        'TYPE': '<b>Type</b>',
+        'ATOM': '<b>Atom</b>',
+        'GROUP': '<b>Group</b>',
+        'SWAP': '<b>SWaP</b>',
+        'TAU_1s': '<b>τ<sub>1s</sub></b>',
+        'REF_NUM': '<b>Reference Number</b>'
+    },
+    text='GROUP',  
+    opacity=1.0,
+)
+
+# Add traces from the scatter plot to the main figure
+fig.add_traces(scatter_tau1s.data)
+
+# Update the marker size and text position for the last added traces
+last_trace_index = len(fig.data) - len(scatter_tau1s.data)
+for i in range(last_trace_index, len(fig.data)):
+    fig.data[i].update(
+        marker=dict(size=12, line=dict(width=0.5, color='DarkSlateGrey')),
+        textposition='top center'
+    )
+
+# Define a shaded region for lab clocks
+shaded_region = go.layout.Shape(
+    type='rect',
+    x0=1e-3,
+    y0=1e-18,
+    x1=1e16,
+    y1=6.7e-17,
+    fillcolor='rgba(0, 0, 255, 0.25)',
+    line=dict(color='rgba(0, 0, 255, 0.1)')
+)
+
+# Add the shaded region to the figure
+fig.add_shape(shaded_region)
+
+# Define the text for the shaded region
+shaded_region_text = (
+    'Lab clock performance @ tau = 1s \n'
+    'NIST - Yb Lattice: 6.7e-17 \n'
+    'Schioppo 2017'
+)
+
+# Add a transparent scatter trace for hover box
+fig.add_trace(go.Scatter(
+    x=[1e-1, 1.e1, 1.e1, 1e-1, 1e-2],
+    y=[1e-17, 1e-17, 2e-17, 2e-17, 1e-17],
+    mode='markers',
+    marker=dict(color='rgba(0, 0, 0, 0)'),
+    hoverinfo='text',
+    text=shaded_region_text,
+    showlegend=False
+))
+
+# Disable legend item click
+fig.update_layout(legend=dict(itemclick=False))
+
+# Configure layout, axis, and other features
+fig.update_layout(
+    xaxis_title='Size, Weight and Power (cm³kgW)',
+    yaxis_title='Instability (s)',
+    yaxis=dict(range=[-17.5, -8]),
+    xaxis=dict(range=[-2.5, 15.5]),
+    xaxis_type='log',
+    yaxis_type='log',
+    xaxis_tickmode='linear',
+    yaxis_tickmode='linear',
+    xaxis_tickformat='.1e',
+    yaxis_tickformat='.1e',
+    legend=dict(
+        yanchor="top",
+        y=1,
+        xanchor="right",
+        x=1,
+        traceorder="reversed",
+    ),
+    annotations=[dict(
+        x=0.5,
+        y=1.09,
+        xref='paper',
+        yref='paper',
+        text='Hover over points on the plot for more information.' +
+             'Use mouse to zoom in, pan or download a PNG of the plot. <br>' +
+             'Scan QR code for more information and source data.',
+        showarrow=False,
+        font=dict(size=12)
+    )]     
+)
+
+# Add annotation for lab clocks area
+fig.add_annotation(
+    x=0,
+    y=-16.8,
+    text="Lab Clock Performance",
+    showarrow=False,
+    align='right',
+    font=dict(size=12, color='rgba(0, 0, 0, 0.8)')
+)
+
+# Add annotation for the author
+fig.add_annotation(
+    xref="paper", yref="paper",
+    x=1,
+    y=0.73,
+    text="B. White 2024",
+    showarrow=False,
+    align='right',
+    font=dict(size=10, color='rgba(0, 0, 0, 0.8)')
+)
+
+# Add annotation for GPS
+fig.add_annotation(
+    x=-1.5,
+    y=-11.6,
+    text="GPS @ 1s",
+    showarrow=False,
+    align='right',
+    font=dict(size=12, color='rgba(0, 0, 0, 0.8)')
+)
+
+# QR code image that links to source data
+# Open the QR code image and convert it to base64
+with open("IPAS-QR.png", "rb") as image_file:
+    base64_image = base64.b64encode(image_file.read()).decode('utf-8')
+
+# Add the QR code image to the plot
+fig.add_layout_image(
+    dict(
+        source='data:image/png;base64,{}'.format(base64_image),
+        xref="paper", yref="paper",
+        x=0.71, y=0.92,
+        sizex=0.15, sizey=0.15,
+        xanchor="center", yanchor="middle",
+        opacity=1.,
+        layer="above"
+    )
+)
+
+# add frame around plot
+fig.update_layout(
+    xaxis=dict(
+        showline=True,
+        linewidth=0.4,
+        mirror=True,
+        linecolor='black'
+    ),
+    yaxis=dict(
+        showline=True,
+        linewidth=0.4,
+        mirror=True,
+        linecolor='black'
+    ),
+    margin=dict(t=70,b=50)
+)
+
+
+
+# Save the plot as an HTML file
+# change output format to PNG, JPEG, SVG, PDF, or EPS as needed
+fig.write_html('main-swap-plot.html')
